f9af82ce33bec2f6664e82b927a1cf4b8cf8243c
Thundros <32390102+Thundros@users.noreply.github.com>
2019-10-17 15:20:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,18 @@
 
 	<!DOCTYPE html>
-
-		<html>
-
-			<meta charset="UTF-8">
-
-				<head>
-
-					<title>Test</title>
-
-					<link rel="stylesheet" href="css/main.css">
-
-				</head>
-
-			<body>
-
-				<script src="src/js/libs/modules/phaser/3.20.1/fitoria/build/phaser.js"></script>
-				<script src="src/js/libs/modules/stats/stats.min.js"></script>
-				<script src="src/js/player.js"></script>
-				<script src="src/js/platformer-scene.js"></script>
-				<script src="src/js/main.js"></script>
-
-			</body>
-
-		</html>
+	<html>
+		<meta charset="UTF-8">
+			<head>
+				<title>Test</title>
+				<link rel="stylesheet" href="css/main.css">
+			</head>
+		<body>
+			<script src="src/js/libs/modules/phaser/3.20.1/fitoria/build/phaser.js"></script>
+			<script src="src/js/libs/modules/stats/stats.min.js"></script>
+			<script src="src/js/player.js"></script>
+			<script src="src/js/platformer-scene.js"></script>
+			<script src="src/js/main.js"></script>
+		</body>
+	</html>
 
 
